7bea66af5c6198cf1214a22ba1162421bbd8178c
yeseric@163.com <yesEric001>
2019-01-14 18:01:29 +0800

    Add factors class

--- src/test/java/ReactorTest.java
+++ src/test/java/ReactorTest.java
@@ -79,4 +79,22 @@ public class ReactorTest {
             System.out.println(t);
         });
     }
+
+    @Test
+    public void testFlatMap(){
+        Flux<Long> fibonacciGenerator=Flux.generate(() -> Tuples.<Long,Long>of(0L,1L),
+                (state,sink) -> {
+                    if(state.getT1()<0){
+                        sink.complete();
+                    }else {
+                        sink.next(state.getT1());
+                    }
+                    return  Tuples.of(state.getT2(),state.getT1()+state.getT2());
+                });
+
+        Factorization numberConvertor=new Factorization();
+        fibonacciGenerator.skip(1).take(10).map(t ->  numberConvertor.findFactor(t.intValue())).subscribe(t->{
+            System.out.println(t);
+        });
+    }
 }
